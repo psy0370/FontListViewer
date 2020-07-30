1d9a1289a052ba56bf7450baaf16f4741d5caa22
psy0370 <52093529+psy0370@users.noreply.github.com>
2020-07-30 20:09:46 +0900

    プロジェクト ファイルを追加します。

---
+++ FontListViewer/Controls/AutoResizeCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FontListViewer.Controls
+{
+    /// <summary>
+    /// コンテンツによっってコントロールサイズが自動調整されるクラスを定義します。
+    /// </summary>
+    public class AutoResizeCanvas : Canvas
+    {
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var baseSize = base.MeasureOverride(constraint);
+            var children = InternalChildren.OfType<UIElement>();
+
+            if(children.Any())
+            {
+                var width = children.Max(x => x.DesiredSize.Width + (double)x.GetValue(LeftProperty));
+                var height = children.Max(x => x.DesiredSize.Height + (double)x.GetValue(TopProperty));
+
+                return new Size(width, height);
+            }
+            else
+            {
+                return baseSize;
+            }
+        }
+    }
+}
---
+++ FontListViewer/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FontListViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ウィンドウの読み込みが完了したときの処理を定義します。
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FontListView.DataContext = Fonts.SystemFontFamilies;
+        }
+
+        /// <summary>
+        /// 選択フォントが変更されたときの処理を定義します。
+        /// </summary>
+        private void FontListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var font = (FontFamily)e.AddedItems[0];
+
+            var y = 0d;
+            FontView.Children.Clear();
+            CreateHeader();
+            CreateParagraph(12);
+            CreateParagraph(18);
+            CreateParagraph(24);
+            CreateParagraph(36);
+            CreateParagraph(48);
+            CreateParagraph(60);
+
+            // ヘッダを描画
+            void CreateHeader()
+            {
+                FontName.Text = $"{font}";
+            }
+
+            // 指定のフォントサイズでサンプル分を描画
+            void CreateParagraph(double size)
+            {
+                var content = new ContentControl();
+                Canvas.SetLeft(content, 0);
+                Canvas.SetTop(content, y);
+
+                var block = new TextBlock()
+                {
+                    FontFamily = font,
+                    FontSize = size
+                };
+                block.Inlines.Add("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+                block.Inlines.Add("123467890.:,;'\"(!?)+-*/=\n");
+                block.Inlines.Add("Windowsでコンピュータの世界が広がります。");
+
+                content.Content = block;
+
+                FontView.Children.Add(content);
+                y += size * font.LineSpacing * 5d;
+            }
+        }
+    }
+}
